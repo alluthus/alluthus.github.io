7558e99200fae901d3e73888acf5a219070fb73b
Joseph Madril <joeymadril@gmail.com>
2020-08-15 00:00:51 -0700

    push

--- assets/js/other.js
+++ assets/js/other.js
@@ -1,25 +1,25 @@
-window.onload = function () {
-    document.getElementById("menus").onclick = function (evt) {
-      bringMenu();
+window.onload = function() {
+    document.getElementById("menus").onclick = function(evt) {
+        bringMenu();
     }
-  
-    document.getElementById("exita1").onclick = function (evt) {
-      outMenu();
+
+    document.getElementById("exita1").onclick = function(evt) {
+        outMenu();
     }
-  }
-  
-  function bringMenu() {
+}
+
+function bringMenu() {
     document.getElementById("dropdowns").style.display = "inline-block";
-    setTimeout(function () {
-      document.getElementById("dropdowns").style.backgroundColor = "rgba(0,0,0,0.5)";
-      document.getElementById("mble").style.right = "0";
+    setTimeout(function() {
+        document.getElementById("dropdowns").style.backgroundColor = "rgba(0,0,0,0.5)";
+        document.getElementById("mble").style.right = "0";
     }, 50);
-  }
-  
-  function outMenu() {
+}
+
+function outMenu() {
     document.getElementById("dropdowns").style.backgroundColor = "rgba(0,0,0,0)";
     document.getElementById("mble").style.right = "-350px";
-    setTimeout(function () {
-      document.getElementById("dropdowns").style.display = "none";
+    setTimeout(function() {
+        document.getElementById("dropdowns").style.display = "none";
     }, 150);
-  }
+}
